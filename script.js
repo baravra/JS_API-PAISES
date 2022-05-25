6c96db61964edd8164d9286f43b3081af452326c
Barbara Leao <87141477+baravra@users.noreply.github.com>
2022-05-25 10:40:19 -0300

    filtrar por continente e buscar pelo nome do país

--- script.js
+++ script.js
@@ -9,18 +9,19 @@ var popTotal = 0
 var globalPaises = []
 var globalFavoritos = []
 var popFavoritos = 0
+var globalRegiao = []
 
 window.addEventListener("load", function(){
     /*-----------------BUSCANDO OS DADOS DA API---------------*/
     const valores = this.fetch('https://restcountries.com/v2/all').then(res=>{
         /*--------------TRANSFORMANDO A RESPOSTA EM JSON---------------*/
             res.json().then(dados=>{
-                //console.log(dados)
                 for(i = 0; i < dados.length ;i++){
                     carregarTela(dados, i)
                 } 
                 divQuantGlobal.innerHTML = "População Total (" + popTotal +")"
                 divPaisesGlobal.innerHTML = "Países (" +i+ ")"
+
             })
         }
     )
@@ -32,12 +33,13 @@ function carregarTela(dados, i){
     var nomePais = dados[i].name
     var quantPss = dados[i].population
     var bandPais = dados[i].flag
+    var regPais = dados[i].region
 
     /*-------------------TEXTO QUE SERA INSERIDO--------------------------*/
     var txt = "<div id='"+codPais+"' class='conteudo'>"
                 + "<img src='"+bandPais+"' alt='bandeira' class='add' onclick='favoritar("+i+")'>"
                     + "<div>"
-                        + "<h3>"+nomePais+"</h3>"
+                        + "<h3 class='"+regPais+"'>"+nomePais+"</h3>"
                         + "<p>"+quantPss+"</p>"
                     + "</div>"
                 + "</div>"
@@ -45,14 +47,14 @@ function carregarTela(dados, i){
     divCarregar.innerHTML += txt
     popTotal += quantPss
 
-    globalPaises.push({nome: nomePais, id: codPais , populacao: quantPss, bandeira: bandPais})
+    globalPaises.push({nome: nomePais, id: codPais , populacao: quantPss, bandeira: bandPais, regiao: regPais})
 }
 
 function favoritar(i){
     var favoritado = globalPaises[i]
 
     /*----------------------ADICIONANDO NO VETOR FAVORITOS E RETIRANDO DO GERAL----------------------*/
-    globalFavoritos.push({nome: favoritado.nome , id: favoritado.id, populacao: favoritado.populacao, bandeira: favoritado.bandeira})
+    globalFavoritos.push({nome: favoritado.nome , id: favoritado.id, populacao: favoritado.populacao, bandeira: favoritado.bandeira, regiao: favoritado.regiao })
     globalPaises.splice(i, 1)
 
    /*----------------------------ATUALIZANDO A RESPOSTA NA TELA------------------------------*/
@@ -67,7 +69,7 @@ function favoritar(i){
        var texto = "<div id='"+pais.id+"' class='conteudo'>"
                     + "<img src='"+pais.bandeira+"' alt='bandeira' class='add' onclick='favoritar("+x+")'>"
                         + "<div>"
-                            + "<h3>"+pais.nome+"</h3>"
+                            + "<h3 class='"+pais.regiao+"'>"+pais.nome+"</h3>"
                             + "<p>"+pais.populacao+"</p>"
                         + "</div>"
                     + "</div>"
@@ -88,7 +90,7 @@ function favoritar(i){
         var textoFav = "<div id='"+paisFavorito.id+"' class='conteudo'>"
                    + "<img src='"+paisFavorito.bandeira+"' alt='bandeira' class='del' onclick='retirar("+y+")'>"
                        + "<div>"
-                           + "<h3>"+paisFavorito.nome+"</h3>"
+                           + "<h3 class='"+paisFavorito.regiao+"'>"+paisFavorito.nome+"</h3>"
                            + "<p>"+paisFavorito.populacao+"</p>"
                        + "</div>"
                    + "</div>"
@@ -105,7 +107,7 @@ function retirar(y){
     
     /*----------------------ADICIONANDO NO VETOR GERAL E RETIRANDO DO FAVORITO----------------------*/
     globalFavoritos.splice(y,1)
-    globalPaises.push({nome: excluido.nome, id: excluido.id, bandeira: excluido.bandeira, populacao: excluido.populacao})
+    globalPaises.push({nome: excluido.nome, id: excluido.id, bandeira: excluido.bandeira, populacao: excluido.populacao, regiao: excluido.regiao})
 
     /*----------------------------ATUALIZANDO A RESPOSTA NA TELA------------------------------*/
     /*---------------------GERAL---------------------------------*/
@@ -118,7 +120,7 @@ function retirar(y){
         var texto = "<div id='"+paisgLOBAL.id+"' class='conteudo'>"
                      + "<img src='"+paisgLOBAL.bandeira+"' alt='bandeira' class='add' onclick='favoritar("+Z+")'>"
                          + "<div>"
-                             + "<h3>"+paisgLOBAL.nome+"</h3>"
+                             + "<h3 class='"+paisgLOBAL.regiao+"'>"+paisgLOBAL.nome+"</h3>"
                              + "<p>"+paisgLOBAL.populacao+"</p>"
                          + "</div>"
                      + "</div>"
@@ -137,7 +139,7 @@ function retirar(y){
          var textoFav = "<div id='"+paisExcluido.id+"' class='conteudo'>"
                     + "<img src='"+paisExcluido.bandeira+"' alt='bandeira' class='del' onclick='retirar("+w+")'>"
                         + "<div>"
-                            + "<h3>"+paisExcluido.nome+"</h3>"
+                            + "<h3  class='"+paisExcluido.regiao+"'>"+paisExcluido.nome+"</h3>"
                             + "<p>"+paisExcluido.populacao+"</p>"
                         + "</div>"
                     + "</div>"
@@ -148,3 +150,132 @@ function retirar(y){
     document.querySelector("#popFavoritos").innerHTML = "População Total (" + popFavoritos + ")"
 }
 
+/*-----------------------------MOSTRAR POR REGIAO--------------------------------*/
+function mostrar(regiao){
+    globalRegiao = []
+    switch(regiao){
+        case "all":
+            divCarregar.innerHTML = ""
+            popTotal = 0
+            globalPaises = globalPaises.sort(function (a, b) {return (a.nome > b.nome) ? 1 : ((b.nome > a.nome) ? -1 : 0);    });
+            for(e = 0; e < globalPaises.length; e++){
+                var PaisAll = globalPaises[e]
+ 
+                var texto = "<div id='"+PaisAll.id+"' class='conteudo'>"
+                            + "<img src='"+PaisAll.bandeira+"' alt='bandeira' class='add' onclick='favoritar("+e+")'>"
+                                + "<div>"
+                                    + "<h3 class='"+PaisAll.regiao+"'>"+PaisAll.nome+"</h3>"
+                                    + "<p>"+PaisAll.populacao+"</p>"
+                                + "</div>"
+                            + "</div>"
+                divCarregar.innerHTML += texto
+                popTotal += PaisAll.populacao
+            }
+            divQuantGlobal.innerHTML = "População Total (" + popTotal +")"
+            divPaisesGlobal.innerHTML = "Países (" +e+ ")"
+        break;    
+        case "Europe":
+            for(a = 0; a < globalPaises.length; a++){
+                if(globalPaises[a].regiao == "Europe"){
+                    globalRegiao.push({nome: globalPaises[a].nome, id: globalPaises[a].id , populacao: globalPaises[a].populacao, bandeira: globalPaises[a].bandeira, regiao: globalPaises[a].regiao})
+                }
+            }
+            montarPesquisa()
+        break;
+        case "Americas":
+            for(a = 0; a < globalPaises.length; a++){
+                if(globalPaises[a].regiao == "Americas"){
+                    globalRegiao.push({nome: globalPaises[a].nome, id: globalPaises[a].id , populacao: globalPaises[a].populacao, bandeira: globalPaises[a].bandeira, regiao: globalPaises[a].regiao})
+                }
+            }
+            montarPesquisa()
+        break;
+        case "Asia":
+            for(a = 0; a < globalPaises.length; a++){
+                if(globalPaises[a].regiao == "Asia"){
+                    globalRegiao.push({nome: globalPaises[a].nome, id: globalPaises[a].id , populacao: globalPaises[a].populacao, bandeira: globalPaises[a].bandeira, regiao: globalPaises[a].regiao})
+                }
+            }
+            montarPesquisa()
+        break;
+        case "Africa":
+            for(a = 0; a < globalPaises.length; a++){
+                if(globalPaises[a].regiao == "Africa"){
+                    globalRegiao.push({nome: globalPaises[a].nome, id: globalPaises[a].id , populacao: globalPaises[a].populacao, bandeira: globalPaises[a].bandeira, regiao: globalPaises[a].regiao})
+                }
+            }
+            montarPesquisa()
+        break;
+        case "Oceania":
+            for(a = 0; a < globalPaises.length; a++){
+                if(globalPaises[a].regiao == "Oceania"){
+                    globalRegiao.push({nome: globalPaises[a].nome, id: globalPaises[a].id , populacao: globalPaises[a].populacao, bandeira: globalPaises[a].bandeira, regiao: globalPaises[a].regiao})
+                }
+            }
+            montarPesquisa()
+        break;
+        case "Polar":
+            for(a = 0; a < globalPaises.length; a++){
+                if(globalPaises[a].regiao == "Polar"){
+                    globalRegiao.push({nome: globalPaises[a].nome, id: globalPaises[a].id , populacao: globalPaises[a].populacao, bandeira: globalPaises[a].bandeira, regiao: globalPaises[a].regiao})
+                }
+            }
+            montarPesquisa()
+        break;
+    }
+}
+
+function montarPesquisa(){
+    divCarregar.innerHTML = ""
+    popTotal = 0
+    globalRegiao = globalRegiao.sort(function (a, b) {return (a.nome > b.nome) ? 1 : ((b.nome > a.nome) ? -1 : 0);    });
+    for(m = 0; m < globalRegiao.length; m++){
+
+        var paisRegiao = globalRegiao[m]
+     
+        var texto = "<div id='"+paisRegiao.id+"' class='conteudo'>"
+                         + "<img src='"+paisRegiao.bandeira+"' alt='bandeira' class='add' onclick='favoritar("+m+")'>"
+                             + "<div>"
+                                 + "<h3 class='"+paisRegiao.regiao+"'>"+paisRegiao.nome+"</h3>"
+                                 + "<p>"+paisRegiao.populacao+"</p>"
+                             + "</div>"
+                         + "</div>"
+        divCarregar.innerHTML += texto
+        popTotal += paisRegiao.populacao
+        
+        
+    }
+    divQuantGlobal.innerHTML = "População Total (" + popTotal +")"
+    divPaisesGlobal.innerHTML = "Países (" +m+ ")"
+}
+
+function busca(){
+    //console.clear()
+    var paisProcura = document.querySelector("#buscar").value
+    divCarregar.innerHTML = ""
+    popTotal =0
+    var teste = 0
+
+    for(b = 0; b<globalPaises.length; b++)    {
+        var nomePaisArray = globalPaises[b];
+
+        if(nomePaisArray.nome.indexOf(paisProcura) != -1){
+
+            var texto = "<div id='"+nomePaisArray.id+"' class='conteudo'>"
+                         + "<img src='"+nomePaisArray.bandeira+"' alt='bandeira' class='add' onclick='favoritar("+b+")'>"
+                             + "<div>"
+                                 + "<h3 class='"+nomePaisArray.regiao+"'>"+nomePaisArray.nome+"</h3>"
+                                 + "<p>"+nomePaisArray.populacao+"</p>"
+                             + "</div>"
+                         + "</div>"
+            divCarregar.innerHTML += texto
+            popTotal += nomePaisArray.populacao
+            teste++
+
+        }
+    }
+    divQuantGlobal.innerHTML = "População Total (" + popTotal +")"
+    divPaisesGlobal.innerHTML = "Países (" +teste+ ")"
+    //console.log(paisProcura)
+
+}
